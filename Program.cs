d7fbbe13ff87f01fbfc9b201aca49d9b25d5829b
MahdiyarZare <mahdiyarzare@yahoo.com>
2023-05-26 23:05:43 +0430

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("please Enter a NUM");
+            string number = Console.ReadLine();
+            num = int.Parse(number);
+            num *= 3;
+            Console.WriteLine(num);
+            Console.ReadKey();
+
+
+        }
+    }
+}
+;
